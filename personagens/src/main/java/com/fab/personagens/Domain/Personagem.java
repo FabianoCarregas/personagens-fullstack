01b9745e290693f8501013b28d0586ef9d52ff64
Fabiano <fabianofac_@hotmail.com>
2021-10-07 09:26:50 -0300

    first commit back

---
+++ personagens/src/main/java/com/fab/personagens/Domain/Personagem.java
@@ -0,0 +1,88 @@
+package com.fab.personagens.Domain;
+
+import java.io.Serializable;
+
+public class Personagem implements Serializable {
+
+    private Long id;
+    private String name;
+    private Integer forca;
+    private String nacionalidade;
+    private String time;
+    private String foto;
+
+    public Personagem(){
+    }
+
+    public Personagem(Long id, String name, Integer forca, String nacionalidade, String time, String foto) {
+        this.id = id;
+        this.name = name;
+        this.forca = forca;
+        this.nacionalidade = nacionalidade;
+        this.time = time;
+        this.foto = foto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getForca() {
+        return forca;
+    }
+
+    public void setForca(Integer forca) {
+        this.forca = forca;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Personagem that = (Personagem) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
